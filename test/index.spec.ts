b99a3d6f4c403a381b248712dd3b918f6d13a048
mishio-n <doncool340@gmail.com>
2021-07-17 00:28:19 +0900

    test: add test

---
+++ test/index.spec.ts
@@ -0,0 +1,47 @@
+import { asyncReader } from "../src";
+
+const StarterPokemonColumn = {
+  fire: 0,
+  water: 1,
+  grass: 2,
+};
+
+describe("test for async-reader", () => {
+  const reader = asyncReader("./test/test.csv", StarterPokemonColumn);
+
+  test("get header", async () => {
+    const header = (await reader.next()).value;
+    expect(header).toStrictEqual({
+      fire: "fire",
+      water: "water",
+      grass: "grass",
+    });
+  });
+
+  test("get no evolved", async () => {
+    const noEvolved = (await reader.next()).value;
+    expect(noEvolved).toStrictEqual({
+      fire: "Charmander",
+      water: "Squirtle",
+      grass: "Bulbasaur",
+    });
+  });
+
+  test("get one evolved", async () => {
+    const oneEvolved = (await reader.next()).value;
+    expect(oneEvolved).toStrictEqual({
+      fire: "Charmeleon",
+      water: "Wartortle",
+      grass: "Ivysaur",
+    });
+  });
+
+  test("get final evolved", async () => {
+    const finalEvolved = (await reader.next()).value;
+    expect(finalEvolved).toStrictEqual({
+      fire: "Chalizard",
+      water: "Blastoise",
+      grass: "Venusaur",
+    });
+  });
+});
